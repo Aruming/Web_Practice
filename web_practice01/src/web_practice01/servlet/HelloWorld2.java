049a6103c7829da4b96146af978136036dd4007b
Aruming <0430kar@naver.com>
2021-10-05 17:44:21 +0900

    GenericServlet 사용
    GenericServlet 사용한 서블릿 서브클래스 구현
    web.xml에 서블릿 선언 및 url 매핑

---
+++ web_practice01/src/web_practice01/servlet/HelloWorld2.java
@@ -0,0 +1,21 @@
+package web_practice01.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class HelloWorld2 extends GenericServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service");
+	}
+
+
+}
